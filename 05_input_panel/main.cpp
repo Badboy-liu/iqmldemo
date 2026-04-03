49acaf45c9df6dfcf307acb1c1099ef70a6c43c4
loukas <1216915972@qq.com>
2026-04-03 22:03:02 +0800

    修复逻辑

--- 05_input_panel/main.cpp
+++ 05_input_panel/main.cpp
@@ -19,13 +19,6 @@
 // ===== main =====
 int main(int argc,char* arg[])
 {
-    QGuiApplication app(argc, arg);
-    QQuickWindow::setGraphicsApi(QSGRendererInterface::OpenGL); // ⭐⭐⭐ 必加
-    // qmlRegisterType<VideoItem>("VideoItem",1,0,"VideoItem");
-    qInstallMessageHandler([](QtMsgType, const QMessageLogContext&, const QString& msg)
-    {
-        std::cout << msg.toStdString() << std::endl;
-    });
     // 1. 获取 vcpkg 根目录
     QString vcpkgRoot = getVcpkgPath();
 
@@ -34,7 +27,6 @@ int main(int argc,char* arg[])
         qCritical() << "无法定位 vcpkg 根目录，程序退出。请确保 'vcpkg' 已添加到系统 PATH 环境变量中。";
         return -1;
     }
-
     // 2. 动态构建子路径 (使用 QDir 确保跨平台分隔符正确)
     // 假设结构: <root>/installed/x64-windows/debug/...
     // 注意：如果你的 vcpkg 是 release 版本或者混合安装，可能需要去掉 "/debug"
@@ -50,11 +42,30 @@ int main(int argc,char* arg[])
     qDebug() << "Plugin Path:" << pluginPath;
     qDebug() << "QML Path:" << qmlPath;
     // ❗ 关键：DLL 依赖路径
+    // qputenv("QT_VIRTUALKEYBOARD_STYLE", "basic");
     qputenv("PATH", QByteArray(binPath.toLocal8Bit() + ";") + qgetenv("PATH"));
+    qputenv("QT_IM_MODULE", "qtvirtualkeyboard");
+    qputenv("QT_VIRTUALKEYBOARD_DESKTOP_DISABLE", "0");
+    qDebug() << "IM module:" << qgetenv("QT_IM_MODULE");
+    QCoreApplication::addLibraryPath(pluginPath);
+    QString imPath = pluginPath + "/platforminputcontexts";
+    QCoreApplication::addLibraryPath(imPath);
+    QGuiApplication app(argc, arg);
+    QQuickWindow::setGraphicsApi(QSGRendererInterface::OpenGL); // ⭐⭐⭐ 必加
+    // QQuickWindow::setGraphicsApi(QSGRendererInterface::Software);
+    // qmlRegisterType<VideoItem>("VideoItem",1,0,"VideoItem");
+    qInstallMessageHandler([](QtMsgType, const QMessageLogContext&, const QString& msg)
+    {
+        std::cout << msg.toStdString() << std::endl;
+    });
 
+
+
+    // qputenv("QT_DEBUG_PLUGINS", "1");
     QQmlApplicationEngine engine;
     engine.addImportPath(qmlPath);
     engine.load(QUrl("qrc:/main.qml"));
+    // engine.addPluginPath(pluginPath);
 
     QObject::connect(
         &engine,
--- 05_input_panel/main.qml
+++ 05_input_panel/main.qml
@@ -1,11 +1,27 @@
 import QtQuick
 import QtQuick.Controls
 import QtQuick.VirtualKeyboard
+import QtQuick.VirtualKeyboard.Settings
+
 Window{
     id:window
     width:640
     height:480
+    visible: true
     title:qsTr("hello world")
+    MouseArea {
+        anchors.fill: parent
+        z: -1   // 在最底层，不影响点击控件
+
+        onClicked: {
+            Qt.inputMethod.hide()   // ⭐关键
+            window.activeFocusItem.focus =false
+        }
+    }
+    Component.onCompleted: {
+        VirtualKeyboardSettings.selectionHandlesVisible = false
+        Qt.inputMethod.show()
+    }
 
     Column{
         anchors.centerIn:parent
@@ -13,10 +29,13 @@ Window{
 
         TextField{
             width:200
+            height:40
             placeholderText:"点击我弹键盘"
+            // focus: true
         }
         TextField{
             width:200
+            height:40
             placeholderText:"再来一个输入框"
         }
     }
@@ -25,9 +44,18 @@ Window{
         id:inputPanel
         z:99
         width:parent.width
-        y:window.height
+        y:visible ? window.height - height : window.height
+        Component.onCompleted: {
+            console.log("IM visible:", Qt.inputMethod.visible)
+        }
+        // ⭐⭐关键：绑定输入法
+        visible: Qt.inputMethod.visible
 
-        states:State{
+        // ⭐ 就加在这里
+        onActiveChanged: {
+            console.log("keyboard active:", active)
+        }
+        states: State {
             name:"visible"
             when:inputPanel.active
             PropertyChanges{
@@ -36,14 +64,14 @@ Window{
             }
         }
 
-        transitions:Transition{
+        transitions: Transition {
             from:""
             to:"visible"
             reversible:true
             NumberAnimation{
                 properties:"y"
                 duration:250
-                easing.type:Easing.InOutQuad
+                easing.type: Easing.InOutQuad
             }
         }
     }
